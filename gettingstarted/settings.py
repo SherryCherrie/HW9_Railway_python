eccbed273ce6254d3b6b2b568c09e3c53e694cf4
Sherry <sherrydevilliers@u.boisestate.edu>
2026-04-20 18:28:28 -0600

    add database url env undo

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -156,7 +156,7 @@ elif IS_RAILWAY_APP:
     # Railway database configuration
     DATABASES = {
         "default": dj_database_url.config(
-            env="JDBC_DATABASE_URL",
+            env="DATABASE_URL",
             conn_max_age=600,
             conn_health_checks=True,
             ssl_require=False,  # Railway handles SSL differently
